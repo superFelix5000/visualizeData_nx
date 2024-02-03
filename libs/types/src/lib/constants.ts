a5006744189e4c4fbfc97b622e95df4d2315ade3
Felix <fwest@posteo.de>
2024-02-03 19:05:03 +0200

    added new year

--- libs/types/src/lib/constants.ts
+++ libs/types/src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const YEARS: number[] = [2017, 2018, 2019, 2020, 2021, 2022, 2023];
+export const YEARS: number[] = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024];
 export const MONTHS = [
     'January',
     'February',
