47bffabd6236869cde3d505bda09f2c83e09a1b0
Felix <fwest@posteo.de>
2024-03-16 09:32:04 +0200

    formatting

--- apps/deno-api/src/main.ts
+++ apps/deno-api/src/main.ts
@@ -1,15 +1,15 @@
-import { Application } from 'https://deno.land/x/oak/mod.ts';
-import { oakCors } from 'https://deno.land/x/cors/mod.ts';
-import router from './routes.ts';
+import { Application } from "https://deno.land/x/oak/mod.ts";
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
+import router from "./routes.ts";
 
 const port = 8000;
 
 const app = new Application();
 
 app.use(
-    oakCors({
-        origin: 'http://localhost:4200',
-    })
+  oakCors({
+    origin: "http://localhost:4200",
+  }),
 );
 app.use(router.routes());
 app.use(router.allowedMethods());
