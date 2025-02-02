5b8a129ea214901bbaa445b2ed0e1cd85e7806cf
FELIX WESTPHAL <foo@bar.de>
2025-02-02 13:04:26 +0200

    added new year

--- libs/types/src/lib/constants.ts
+++ libs/types/src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const YEARS: number[] = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]; //TODO: fetch years from data automagically?
+export const YEARS: number[] = [2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024, 2025]; //TODO: fetch years from data automagically?
 export const MONTHS = [
     'January',
     'February',
